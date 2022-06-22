7df4081cd42d4ae985ee2db17673e81de1305832
Yourui <70494538+yshao0712@users.noreply.github.com>
2022-06-21 20:09:40 -0700

    Update FLO_water.py
    Connected to MySQL database

--- FLO_water.py
+++ FLO_water.py
@@ -1,55 +1,46 @@
 import os
-import sys
+from datetime import datetime, timezone
 import pprint
-import logging
-import json
+import time
 
+import mysql.connector
 from pyflowater import PyFlo
 
-def setup_logger():
-    logger = logging.getLogger()
-    logger.setLevel(logging.DEBUG)
+cnx = mysql.connector.connect(user='root', password=os.getenv('MYSQL_PASSWORD', None),
+                              host='127.0.0.1',
+                              database='FLO_water')
+cursor = cnx.cursor()
 
-    handler = logging.StreamHandler(sys.stdout)
-    handler.setLevel(logging.DEBUG)
+user = os.getenv('FLO_USER', None)
+password = os.getenv('FLO_PASSWORD', None)
+py_flo = PyFlo(user, password)
 
-    formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-
-def main():
-    user = os.getenv('FLO_USER', None)
-    password = os.getenv('FLO_PASSWORD', None)
-
-    if (user == None) or (password == None):
-        print("ERROR! Must define env variables FLO_USER and FLO_PASSWORD")
-        raise SystemExit
-
-    pp = pprint.PrettyPrinter(indent = 2)
- 
-    flo = PyFlo(user, password)
-
-    print(f"User = #{flo.user_id}")
-
+def adddata(flo):
     locations = flo.locations()
 
     for location in locations:
         for device in location['devices']:
             id = device['id']
-            print("device id:",id)
-
             deviceinfo = flo.device(id)
 
             telemetry = deviceinfo['telemetry']
             current = telemetry['current']
             psi = current['psi']
             gpm = current['gpm']
-            time = current['updated']
+            
+            timestamp = current['updated']
+            dt_timestamp = datetime.strptime(timestamp,'%Y-%m-%dT%XZ')
+            cvt_dt_timestamp = dt_timestamp.replace(tzinfo=timezone.utc).astimezone(tz=None)
+            new_timestamp = cvt_dt_timestamp.strftime("%Y-%m-%d %X")
 
-            print("psi:",psi)
-            print("gpm:",gpm)
-            print("timestamp:",time)
-
+            add_data = "INSERT INTO water_usage VALUES(\'"+new_timestamp+"\',"+str(psi)+","+str(gpm)+");"
+            print(add_data)
+            cursor.execute(add_data)
+            cnx.commit()
 
 if __name__ == "__main__":
-    main()
+    i = 0
+    while i < 50000:
+        time.sleep(5)
+        adddata(py_flo)
+        i+1
