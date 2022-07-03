737ff97595053e33f9e964e0ecaef322c7686c88
yshao0712 <70494538+yshao0712@users.noreply.github.com>
2022-07-02 21:43:54 -0700

    Update FLO_water_database.sql

--- FLO_water_database.sql
+++ FLO_water_database.sql
@@ -1,4 +1,6 @@
 CREATE DATABASE FLO_water;
 USE FLO_water;
 
-CREATE TABLE water_usage (timestamp timestamp, psi decimal(15,5), gpm decimal(15,5));
+CREATE TABLE water_usage (id INT AUTO_INCREMENT primary key NOT NULL, timestamp timestamp, psi decimal(15,5), gpm decimal(15,5));
+ALTER TABLE water_usage ALTER psi SET DEFAULT 0;
+ALTER TABLE water_usage ALTER gpm SET DEFAULT 0;
